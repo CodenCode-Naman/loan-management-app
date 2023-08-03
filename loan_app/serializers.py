1920fde9b4e24a961421cabd9d28b45e5f491884
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-03 11:17:30 +0530

    feat: add Loan Detail and Loan Serializers

--- loan_app/serializers.py
+++ loan_app/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import Customer
+from .models import Customer, Loan, LoanDetail
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ("name", "email_id", "annual_income", "aadhar_id")
+
+
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Loan
+        fields = ("loan_type", "interest_rate")
+
+
+class LoanDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LoanDetail
+        fields = ["loan_id"]
