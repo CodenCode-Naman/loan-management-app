ca493e9d1c5363c2d4cdcf76c47b79d37c35673b
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-03 11:45:35 +0530

    fix: add admin site to map with models

--- loan_app/admin.py
+++ loan_app/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 admin.site.register(models.Customer)
+admin.site.register(models.AccountTransaction)
+admin.site.register(models.Loan)
+admin.site.register(models.LoanDetail)
+admin.site.register(models.Transaction)
