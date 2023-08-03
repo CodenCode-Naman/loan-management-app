88487c75645471d6db1c4b7aaf7b3b4cdf6bddac
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-03 11:44:42 +0530

    feat: add request url end points for views

--- loan_app/urls.py
+++ loan_app/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, re_path
+
 from loan_app.views import (
     CustomerView,
+    LoanApplicationView,
+    LoanPaymentView,
+    LoanStatementView,
 )
 
 urlpatterns = [
     path("register-user/", CustomerView.as_view(), name="register_user"),
-
+    path("apply-loan/", LoanApplicationView.as_view(), name="apply_loan"),
+    path("make-payment/", LoanPaymentView.as_view(), name="make_payment"),
+    path("get-statement/", LoanStatementView.as_view(), name="get_statement"),
 ]
