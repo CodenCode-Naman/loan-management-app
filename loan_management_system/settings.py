e43ab23acca10d45bdff2f25ca4b19cd62419308
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-03 10:38:04 +0530

    feat: add auth token url to loan management system

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_THROTTLE_RATES": {
-        "anon": "100/day",
         "user": "1000/day",
+        "anon": "100/day",
     },
 }
